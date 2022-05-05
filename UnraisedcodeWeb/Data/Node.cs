fa5485ed5194ed5dac80373503e4af090b4f60dd
César Intriago <46511735+ForerunnerG34@users.noreply.github.com>
2022-05-05 12:08:50 -0500

    Version 1.0

---
+++ UnraisedcodeWeb/Data/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace UnraisedcodeWeb.Data
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node? Next { get; set; }
+        public double Position { get; set; }
+        public string ArrowDirection { get; set; } = string.Empty;
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+}
